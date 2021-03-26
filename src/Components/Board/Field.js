1f2ed37b7da88299748b00acbfd954536cbb3ae2
gapinska <gapinska.mon@gmail.com>
2021-03-26 22:27:47 +0100

    format refactor

--- src/Components/Board/Field.js
+++ src/Components/Board/Field.js
@@ -1,28 +1,32 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Paper from "@material-ui/core/Paper"
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		'& > *': {
-			margin: theme.spacing(0.2),
-			width: theme.spacing(12),
-			height: theme.spacing(12)
-		}
-	}
+  root: {
+    "& > *": {
+      margin: theme.spacing(0.2),
+      width: theme.spacing(12),
+      height: theme.spacing(12),
+    },
+  },
 }))
 
 const Field = ({ index, onClick, value }) => {
-	console.log('RENDER')
+  console.log("RENDER")
 
-	const classes = useStyles()
-	return (
-		<div className={classes.root}>
-			<Paper elevation={3} className="field" onClick={() => onClick(index, value)}>
-				{value}
-			</Paper>
-		</div>
-	)
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <Paper
+        elevation={3}
+        className="field"
+        onClick={() => onClick(index, value)}
+      >
+        {value}
+      </Paper>
+    </div>
+  )
 }
 
 export default React.memo(Field)
--- src/boardLogic.js
+++ src/boardLogic.js
@@ -1,59 +1,59 @@
 function createBoardLogic(N) {
-	const horizontalLines = calculateHorizontalWinnerLines()
+  const horizontalLines = calculateHorizontalWinnerLines()
 
-	function calculateWinnerLines() {
-		return [
-			...horizontalLines,
-			...calculateVerticalWinnerLines(horizontalLines),
-			...calculateDiagonalWinnerLines(horizontalLines)
-		]
-	}
+  function calculateWinnerLines() {
+    return [
+      ...horizontalLines,
+      ...calculateVerticalWinnerLines(horizontalLines),
+      ...calculateDiagonalWinnerLines(horizontalLines),
+    ]
+  }
 
-	function calculateHorizontalWinnerLines() {
-		const horizontalLines = createEmptyBoard()
-		let index = 0
-		for (let row = 0; row < N; row++) {
-			for (let col = 0; col < N; col++) {
-				if (index < Math.pow(N, N)) {
-					horizontalLines[row][col] = index++
-				}
-			}
-		}
-		return horizontalLines
-	}
+  function calculateHorizontalWinnerLines() {
+    const horizontalLines = createEmptyBoard()
+    let index = 0
+    for (let row = 0; row < N; row++) {
+      for (let col = 0; col < N; col++) {
+        if (index < Math.pow(N, N)) {
+          horizontalLines[row][col] = index++
+        }
+      }
+    }
+    return horizontalLines
+  }
 
-	function calculateVerticalWinnerLines(horizontalLines) {
-		const verticalWinnerLines = Array.from(Array(N), () => new Array(N))
-		for (let row = 0; row < N; row++) {
-			for (let col = 0; col < N; col++) {
-				verticalWinnerLines[row][col] = horizontalLines[col][row]
-			}
-		}
-		return verticalWinnerLines
-	}
+  function calculateVerticalWinnerLines(horizontalLines) {
+    const verticalWinnerLines = Array.from(Array(N), () => new Array(N))
+    for (let row = 0; row < N; row++) {
+      for (let col = 0; col < N; col++) {
+        verticalWinnerLines[row][col] = horizontalLines[col][row]
+      }
+    }
+    return verticalWinnerLines
+  }
 
-	function calculateDiagonalWinnerLines(horizontalLines) {
-		const diagonal1WinnerLines = Array(N)
-		for (let row = 0; row < N; row++) {
-			diagonal1WinnerLines[row] = horizontalLines[row][row]
-		}
-		const diagonal2WinnerLines = Array(N)
-		let acc = N - 1
-		for (let row = 0; row < N; row++) {
-			diagonal2WinnerLines[row] = horizontalLines[row][acc]
-			acc--
-		}
-		return [ diagonal1WinnerLines, diagonal2WinnerLines ]
-	}
+  function calculateDiagonalWinnerLines(horizontalLines) {
+    const diagonal1WinnerLines = Array(N)
+    for (let row = 0; row < N; row++) {
+      diagonal1WinnerLines[row] = horizontalLines[row][row]
+    }
+    const diagonal2WinnerLines = Array(N)
+    let acc = N - 1
+    for (let row = 0; row < N; row++) {
+      diagonal2WinnerLines[row] = horizontalLines[row][acc]
+      acc--
+    }
+    return [diagonal1WinnerLines, diagonal2WinnerLines]
+  }
 
-	function createEmptyBoard() {
-		return Array.from(Array(N), () => new Array(N).fill(null))
-	}
+  function createEmptyBoard() {
+    return Array.from(Array(N), () => new Array(N).fill(null))
+  }
 
-	return {
-		calculateWinnerLines,
-		createEmptyBoard
-	}
+  return {
+    calculateWinnerLines,
+    createEmptyBoard,
+  }
 }
 
 export { createBoardLogic }
