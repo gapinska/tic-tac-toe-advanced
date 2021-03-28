8e11c43f839af9fd92a0efc3b928b091eec5478c
gapinska <gapinska.mon@gmail.com>
2021-03-28 22:46:07 +0200

    Board component refactor

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react"
+import React, { useState, useEffect, useCallback, useReducer } from "react"
 import Field from "./Field"
 import GameStart from "../GameFlow/GameStart"
 import { calculateVerdict, X, O, TIE } from "../../gameLogic"
@@ -13,7 +13,66 @@ import GameRestart from "../GameFlow/GameRestart"
 import { createBoardLogic } from "../../boardLogic"
 import { makeStyles } from "@material-ui/core/styles"
 import TextField from "@material-ui/core/TextField"
-import { calculateWinner } from "../../gameLogic"
+
+const ACTIONS = {
+  SET_START_GAME: "setStartGame",
+  SET_GAMER1_STARTED: "setGamer1Started",
+  SET_CONTINUE_GAME_ACTIVE: "setContinueGameActive",
+  SET_END_GAME: "setEndGame",
+  SET_VERDICT: "setVerdict",
+  SET_GAMER1_SCORE: "setGamer1Score",
+  SET_GAMER2_SCORE: "setGamer2Score",
+  SET_MODAL_IS_OPEN: "setModalIsOpen",
+  SET_BOARD_SIZE: "setBoardSize",
+  SET_BOARD_LOGIC: "setBoardLogic",
+  SET_WINNER_LINES: "setWinnerLines",
+}
+
+const initialState = {
+  startGame: false,
+  gamer1Started: true,
+  continueGameActive: false,
+  endGame: false,
+  boardFields: Array(9).fill(null),
+  gamer1Turn: true,
+  verdict: null,
+  gamer1Score: 0,
+  gamer2Score: 0,
+  modalIsOpen: false,
+  boardSize: 3,
+  boardLogic: null,
+  winnerLines: [],
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.SET_BOARD_LOGIC:
+      return { ...state, boardLogic: createBoardLogic(state.boardSize) }
+    case ACTIONS.SET_WINNER_LINES:
+      return { ...state, winnerLines: state.boardLogic.calculateWinnerLines() }
+    case ACTIONS.SET_GAMER1_SCORE:
+      return { ...state, gamer1Score: action.payload }
+    case ACTIONS.SET_GAMER2_SCORE:
+      return { ...state, gamer2Score: action.payload }
+    case ACTIONS.SET_VERDICT:
+      return { ...state, verdict: action.payload }
+    case ACTIONS.SET_CONTINUE_GAME_ACTIVE:
+      return { ...state, continueGameActive: action.payload }
+    case ACTIONS.SET_MODAL_IS_OPEN:
+      return { ...state, modalIsOpen: action.payload }
+    case ACTIONS.SET_GAMER1_STARTED:
+      return { ...state, gamer1Started: !state.gamer1Started }
+    case ACTIONS.SET_START_GAME:
+      return { ...state, startGame: action.payload }
+    case ACTIONS.SET_BOARD_SIZE:
+      return { ...state, boardSize: action.payload }
+    case ACTIONS.SET_END_GAME:
+      return { ...state, endGame: true }
+
+    default:
+      return state
+  }
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,52 +84,52 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const Board = () => {
-  const [startGame, setStartGame] = useState(false)
-  const [gamer1Started, setgamer1Started] = useState(true)
-  const [continueGameActive, setContinueGameActive] = useState(false)
-  const [endGame, setEndGame] = useState(false)
+  const [state, dispatch] = useReducer(reducer, initialState)
   const [boardFields, setBoardFields] = useState(Array(9).fill(null))
   const [gamer1Turn, setGamer1Turn] = useState(true)
-  const [verdict, setVerdict] = useState(null)
-  const [gamer1Score, setGamer1Score] = useState(0)
-  const [gamer2Score, setGamer2Score] = useState(0)
-  const [modalIsOpen, setModalIsOpen] = useState(false)
-  const [boardSize, setBoardSize] = useState(3)
-  const [boardLogic, setBoardLogic] = useState(null)
-  const [winnerLines, setWinnerLines] = useState([])
   const classes = useStyles()
 
   useEffect(() => {
-    setBoardLogic(createBoardLogic(boardSize))
-  }, [boardSize])
+    dispatch({ type: ACTIONS.SET_BOARD_LOGIC })
+  }, [state.boardSize])
 
   useEffect(() => {
-    if (boardLogic !== null) {
-      setWinnerLines(boardLogic.calculateWinnerLines())
-      setBoardFields(boardLogic.createInitialBoard(boardSize))
+    if (state.boardLogic !== null) {
+      dispatch({ type: ACTIONS.SET_WINNER_LINES })
+      setBoardFields(state.boardLogic.createInitialBoard(state.boardSize))
     }
-  }, [boardLogic])
+  }, [state.boardLogic])
 
   useEffect(() => {
-    if (boardLogic) {
-      const verdict = calculateVerdict(boardFields, winnerLines, boardSize)
+    if (state.boardLogic) {
+      const verdict = calculateVerdict(
+        boardFields,
+        state.winnerLines,
+        state.boardSize
+      )
       switch (verdict) {
         case X:
-          setGamer1Score((prevGamer1Score) => prevGamer1Score + 1)
-          setVerdict(verdict)
-          setContinueGameActive(true)
-          setModalIsOpen(true)
+          dispatch({
+            type: ACTIONS.SET_GAMER1_SCORE,
+            payload: state.gamer1Score + 1,
+          })
+          dispatch({ type: ACTIONS.SET_VERDICT, payload: verdict })
+          dispatch({ type: ACTIONS.SET_CONTINUE_GAME_ACTIVE, payload: true })
+          dispatch({ type: ACTIONS.SET_MODAL_IS_OPEN, payload: true })
           break
         case O:
-          setGamer2Score((prevGamer1Score) => prevGamer1Score + 1)
-          setVerdict(verdict)
-          setContinueGameActive(true)
-          setModalIsOpen(true)
+          dispatch({
+            type: ACTIONS.SET_GAMER2_SCORE,
+            payload: state.gamer2Score + 1,
+          })
+          dispatch({ type: ACTIONS.SET_VERDICT, payload: verdict })
+          dispatch({ type: ACTIONS.SET_CONTINUE_GAME_ACTIVE, payload: true })
+          dispatch({ type: ACTIONS.SET_MODAL_IS_OPEN, payload: true })
           break
         case TIE:
-          setVerdict(TIE)
-          setContinueGameActive(true)
-          setModalIsOpen(true)
+          dispatch({ type: ACTIONS.SET_VERDICT, payload: TIE })
+          dispatch({ type: ACTIONS.SET_CONTINUE_GAME_ACTIVE, payload: true })
+          dispatch({ type: ACTIONS.SET_MODAL_IS_OPEN, payload: true })
           break
         default:
           return
@@ -94,45 +153,47 @@ const Board = () => {
   const handleClickGamerPicked = (gamer) => {
     if (gamer !== X) {
       setGamer1Turn(false)
-      setgamer1Started(false)
+      dispatch({ type: ACTIONS.SET_GAMER1_STARTED })
     }
-    setStartGame(true)
+    dispatch({ type: ACTIONS.SET_START_GAME, payload: true })
   }
 
   const handleClickContinueGame = () => {
-    if (continueGameActive) {
-      setGamer1Turn(!gamer1Started)
-      setgamer1Started(!gamer1Started)
-      setBoardFields(boardLogic.createInitialBoard(boardSize))
-      setVerdict(null)
-      setContinueGameActive(false)
-      setModalIsOpen(false)
+    if (state.continueGameActive) {
+      setGamer1Turn(!state.gamer1Started)
+      dispatch({ type: ACTIONS.SET_GAMER1_STARTED })
+      setBoardFields(state.boardLogic.createInitialBoard(state.boardSize))
+      dispatch({ type: ACTIONS.SET_VERDICT, payload: null })
+      dispatch({ type: ACTIONS.SET_CONTINUE_GAME_ACTIVE, payload: true })
+      dispatch({ type: ACTIONS.SET_MODAL_IS_OPEN, payload: false })
     }
   }
 
   const handleClickRestartGame = () => {
-    setStartGame(false)
+    dispatch({ type: ACTIONS.SET_START_GAME, payload: false })
     setBoardFields(Array(9).fill(null))
-    setGamer1Score(0)
-    setGamer2Score(0)
-    setVerdict(null)
-    setModalIsOpen(false)
-    setBoardSize(3)
+    dispatch({ type: ACTIONS.SET_GAMER1_SCORE, payload: 0 })
+    dispatch({ type: ACTIONS.SET_GAMER2_SCORE, payload: 0 })
+    dispatch({ type: ACTIONS.SET_VERDICT, payload: null })
+    dispatch({ type: ACTIONS.SET_MODAL_IS_OPEN, payload: false })
+    dispatch({ type: ACTIONS.SET_BOARD_SIZE, payload: 3 })
   }
 
   const handleClickEndGame = () => {
-    setEndGame(true)
+    dispatch({ type: ACTIONS.SET_END_GAME })
   }
 
   const gamerTurn = gamer1Turn ? X : O
 
   const handleChangeBoardSize = (event) => {
-    setBoardSize(parseInt(event.target.value) || 3)
+    dispatch({
+      type: ACTIONS.SET_BOARD_SIZE,
+      payload: parseInt(event.target.value) || 3,
+    })
   }
-
   return (
     <div>
-      {(!startGame && (
+      {(!state.startGame && (
         <div>
           <GameStart />
           <form className={classes.root} noValidate autoComplete="off">
@@ -153,22 +214,27 @@ const Board = () => {
           </div>
         </div>
       )) ||
-        (startGame && !endGame && (
+        (state.startGame && !state.endGame && (
           <div>
-            <GameBar gamer1Score={gamer1Score} gamer2Score={gamer2Score} />
+            <GameBar
+              gamer1Score={state.gamer1Score}
+              gamer2Score={state.gamer2Score}
+            />
             <div className="game-flow-btn-section">
               <GameContinue
-                status={verdict ? true : false}
+                status={state.verdict ? true : false}
                 handleClickContinueGame={handleClickContinueGame}
               />
               <GameRestart handleClickRestartGame={handleClickRestartGame} />
               <GameEnd handleClickEndGame={handleClickEndGame} />
             </div>
-            <GameStatus gamerTurn={gamerTurn} verdict={verdict} />
+            <GameStatus gamerTurn={gamerTurn} verdict={state.verdict} />
             <div className="game-board">
               <div
                 className="board"
-                style={{ gridTemplateColumns: `repeat(${boardSize}, 1fr)` }}
+                style={{
+                  gridTemplateColumns: `repeat(${state.boardSize}, 1fr)`,
+                }}
               >
                 {boardFields.map((boardField, index) => (
                   <Field
@@ -180,9 +246,14 @@ const Board = () => {
                 ))}
               </div>
             </div>
-            <VerdictModal isOpen={modalIsOpen} />
+            <VerdictModal isOpen={state.modalIsOpen} />
           </div>
-        )) || <GameOver gamer1Score={gamer1Score} gamer2Score={gamer2Score} />}
+        )) || (
+          <GameOver
+            gamer1Score={state.gamer1Score}
+            gamer2Score={state.gamer2Score}
+          />
+        )}
     </div>
   )
 }
